b39bf242f7a9d87799f37d2c9b49eca573d2379d
Nandini1254 <70842224+Nandini1254@users.noreply.github.com>
2020-10-10 12:45:50 +0530

    upadte,file.c
    I try to find element througth binary search  in this program

--- simplelinklist.c
+++ simplelinklist.c
@@ -1,5 +1,80 @@
 //https://www.facebook.com/devesh.pareek.7503/posts/139817487824979
 //subcribe by devesh pareek
+//https://www.facebook.com/devesh.pareek.7503/posts/139817487824979
+//subcribe by devesh pareek
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+void sort_array(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+} 
+int find_value(int *arr,int n,int data)
+{
+    int mid;
+    int l=0,r=n-1;
+    while(l<=r)
+    {
+       // printf("%d ",l);
+         mid=(l+r)/2;
+        if(data==arr[mid])
+            return mid+1;
+        else if(data<arr[mid])
+            r=mid-1;
+        else if(data>arr[mid])
+            l=mid+1;
+    }
+   // printf("%d",mid);
+    if(l>r)
+      return -1;
+    else
+      return mid+1;
+    
+}
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n,m;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]<arr[i-1])
+        {
+            printf("Numbers are not in ascending order");
+            exit(0);
+        }
+    }
+    sort_array(arr,n);
+      printf("\n enter element which you want to find");
+    scanf("%d",&m);
+   // printf("%d",m);
+        int y=find_value(arr,n,m);
+       if(y==-1)
+       {
+           printf("item not found\n");
+           exit(0);
+       }
+    printf("item found and item is %d at position = %d ", arr[y-1],y);
+ 
+    return 0;
+}
+
+/*
 #include<stdio.h>
 int main()
 {
@@ -13,7 +88,7 @@ int main()
     {
         printf("%d",a[i]);
     }
-    printf("\n enter element which you want to find");
+  
     scanf("%d",&found);
     for(i=0;i<n;i++)
     {
@@ -23,9 +98,5 @@ int main()
              loc=1;
         } 
         
-    }if (flag == 1 ){
-    printf("item found and item is %d at position = %d ", a[loc],loc+1);}
-    else{  
-     printf("item not found");
-     }  
-}    
+    
+}    */
